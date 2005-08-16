042f0e7ed1df2d493490d0ab5d5021e493ccb38c
Marcin Konarski <marcin.konarski@codestation.org>
2005-08-16 08:13:16 +0000

    r1007: Recommit of lost changes.

---
+++ src/tut_stdhapi_hcore_hlist.c
@@ -0,0 +1,68 @@
+/*
+---            `tress' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski             ---
+
+	tut.c - this file is integral part of `tress' project.
+
+	i.  You may not make any changes in Copyright information.
+	ii. You must attach Copyright information to any part of every copy
+	    of this software.
+
+Copyright:
+
+ You are free to use this program as is, you can redistribute binary
+ package freely but:
+  1. You can not use any part of sources of this software.
+  2. You can not redistribute any part of sources of this software.
+  3. No reverse engineering is allowed.
+  4. If you want redistribute binary package you can not demand any fees
+     for this software.
+     You can not even demand cost of the carrier (CD for example).
+  5. You can not include it to any commercial enterprise (for example 
+     as a free add-on to payed software or payed newspaper).
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
+*/
+
+#include "TUT/tut.h"
+
+#include <stdhapi.h>
+M_CVSID ( "$CVSHeader$" );
+
+using namespace tut;
+using namespace std;
+using namespace stdhapi;
+using namespace stdhapi::hcore;
+using namespace stdhapi::hconsole;
+using namespace stdhapi::tools;
+using namespace stdhapi::tools::util;
+
+namespace tut
+{
+
+struct tut_stdhapi_hcore_hlist
+	{
+	};
+
+typedef test_group < tut_stdhapi_hcore_hlist > tut_group;
+typedef tut_group::object module;
+tut_group tut_stdhapi_hcore_hlist_group ( "stdhapi::hcore::HList" );
+
+template < >
+template < >
+void module::test<1> ( void )
+	{
+	HList < int > l_oList;
+	l_oList.add_tail ( ) = 3;
+	l_oList.add_tail ( ) = 1;
+	l_oList.add_tail ( ) = 2;
+	ensure_equals ( "operator[] or add_tail failed", l_oList [ 0 ], 3 );
+	ensure_equals ( "operator[] or add_tail failed", l_oList [ 1 ], 1 );
+	ensure_equals ( "operator[] or add_tail failed", l_oList [ 2 ], 2 );
+	l_oList.sort_by_contents ( );
+	ensure_equals ( "sort failed", l_oList [ 0 ], 1 );
+	ensure_equals ( "sort failed", l_oList [ 1 ], 2 );
+	ensure_equals ( "sort failed", l_oList [ 2 ], 3 );
+	}
+
+}
