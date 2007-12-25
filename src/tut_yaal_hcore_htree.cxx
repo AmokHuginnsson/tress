e0842e2dd68601cedb6d88d1ad81726f05844f48
Marcin Konarski <marcin.konarski@codestation.org>
2007-12-25 01:16:48 +0000

    r2508: basic tests for HTree<>

--- src/tut_yaal_hcore_htree.cxx
+++ src/tut_yaal_hcore_htree.cxx
@@ -40,23 +40,108 @@ using namespace yaal::tools::util;
 namespace tut
 {
 
-#if 0
-
-struct tut_
+struct tut_yaal_hcore_htree
 	{
+	static HString _cache;
+	typedef HTree<char> tree_t;
+	static HString& to_string( tree_t const& );
+	static void to_string( tree_t::HNode const& );
+	static void draw_tree( tree_t const& );
+	static void draw_node( tree_t::HNode const& );
 	};
 
-typedef test_group<tut_> tut_group;
-typedef tut_group::object module;
-tut_group tut__group( "yaal::" );
+HString tut_yaal_hcore_htree::_cache;
 
+HString& tut_yaal_hcore_htree::to_string( tree_t const& t )
+	{
+	_cache.clear();
+	to_string( *t.get_root() );
+	return ( _cache );
+	}
+
+void tut_yaal_hcore_htree::to_string( tree_t::HNode const& n )
+	{
+	_cache += *n;
+	for ( tree_t::iterator it = n.begin(); it != n.end(); ++ it )
+		to_string( *it );
+	}
+
+void tut_yaal_hcore_htree::draw_tree( tree_t const& t )
+	{
+	_cache.clear();
+	draw_node( *t.get_root() );
+	return;
+	}
+
+void tut_yaal_hcore_htree::draw_node( tree_t::HNode const& n )
+	{
+	cout << _cache << *n << endl;
+	for ( tree_t::iterator it = n.begin(); it != n.end(); ++ it )
+		draw_node( *it );
+	}
+
+typedef test_group<tut_yaal_hcore_htree> tut_group;
+typedef tut_group::object module;
+tut_group tut_yaal_hcore_htree_group( "yaal::hcore::HTree" );
+
+/* trivial constructor */
 template<>
 template<>
 void module::test<1>( void )
 	{
+	tree_t t;
+	ensure_equals( "new tree not clear", t.get_root(), static_cast<tree_t::node_t>( NULL ) );
 	}
 
-#endif
+/* creating root */
+template<>
+template<>
+void module::test<2>( void )
+	{
+	tree_t t;
+	ensure_equals( "new tree not clear", t.get_root(), static_cast<tree_t::node_t>( NULL ) );
+	tree_t::node_t n = t.set_new_root();
+	ensure( "root not created", n );
+	ensure_equals( "new root is invalid", t.get_root(), n );
+	}
+
+/* setting getting values */
+template<>
+template<>
+void module::test<3>( void )
+	{
+	tree_t t;
+	tree_t::node_t n = t.set_new_root();
+	(**n) = 'x';
+	ensure_equals( "value set/get failed", **n, 'x' );
+	}
+
+/* get_parent */
+template<>
+template<>
+void module::test<4>( void )
+	{
+	tree_t t;
+	tree_t::node_t n = t.set_new_root();
+	ensure_equals( "root node malformed", n->get_parent(), static_cast<tree_t::node_t>( NULL ) );
+	}
+
+/* add_node */
+template<>
+template<>
+void module::test<5>( void )
+	{
+	tree_t t;
+	tree_t::node_t n = t.set_new_root();
+	(**n) = '0';
+	tree_t::iterator it = n->add_node( 'x' );
+	n->add_node( 'y' );
+	it->add_node( 'A' );
+	it->add_node( 'B' );
+	it->insert_node( it->begin(), 'Q' );
+	cout << endl << to_string( t ) << endl;
+	draw_tree( t );
+	}
 
 }
 
