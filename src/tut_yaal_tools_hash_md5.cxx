10fdc332bb7549da59106944a39a895c350deee1
Marcin Konarski <marcin.konarski@codestation.org>
2008-07-22 20:46:26 +0200

    tests for md5 hashing function.

---
+++ src/tut_yaal_tools_hash_md5.cxx
@@ -0,0 +1,55 @@
+/*
+---            `tress' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski             ---
+
+	tut.cxx - this file is integral part of `tress' project.
+
+	i.  You may not make any changes in Copyright information.
+	ii. You must attach Copyright information to any part of every copy
+	    of this software.
+
+Copyright:
+
+ You are free to use this program as is, you can redistribute binary
+ package freely but:
+  1. You can not use any part of sources of this software.
+  2. You can not redistribute any part of sources of this software.
+  3. No reverse engineering is allowed.
+  4. If you want redistribute binary package you can not demand any fees
+     for this software.
+     You can not even demand cost of the carrier (CD for example).
+  5. You can not include it to any commercial enterprise (for example 
+     as a free add-on to payed software or payed newspaper).
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
+*/
+
+#include <TUT/tut.h>
+
+#include <yaal/yaal.h>
+M_VCSID( "$Id: "__ID__" $" )
+
+using namespace tut;
+using namespace std;
+using namespace yaal;
+using namespace yaal::hcore;
+using namespace yaal::hconsole;
+using namespace yaal::tools;
+using namespace yaal::tools::util;
+using namespace tress::tut_helpers;
+
+namespace tut
+{
+
+struct tut_yaal_tools_hash_md5
+	{
+	};
+
+TUT_TEST_GROUP( tut_yaal_tools_hash_md5, "yaal::tools::hash::md5" );
+
+TUT_UNIT_TEST( "from file" )
+	cout << hash::md5( HBitSourceFile( "./data/karatsuba.bc" ) ) << endl;
+TUT_TEARDOWN()
+
+}
+
