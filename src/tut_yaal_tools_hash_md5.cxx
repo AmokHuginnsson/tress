187ecb3439291dbcd5e456cfd7d698be80cc59ae
Marcin Konarski <marcin.konarski@codestation.org>
2008-07-23 18:00:11 +0200

    test suita

--- src/tut_yaal_tools_hash_md5.cxx
+++ src/tut_yaal_tools_hash_md5.cxx
@@ -59,6 +59,18 @@ TUT_UNIT_TEST( "from string (small alternation)" )
 	ensure_equals( "bad hash", hash::md5( HBitSourceMemory( D_INPUT, sizeof ( D_INPUT ) - 1 ) ), D_HASH );
 TUT_TEARDOWN()
 
+#if 0
+MD5 ("") = d41d8cd98f00b204e9800998ecf8427e
+MD5 ("a") = 0cc175b9c0f1b6a831c399e269772661
+MD5 ("abc") = 900150983cd24fb0d6963f7d28e17f72
+MD5 ("message digest") = f96b697d7cb7938d525a2f31aaf161d0
+MD5 ("abcdefghijklmnopqrstuvwxyz") = c3fcd3d76192e4007dfb496cca67e13b
+MD5 ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789") =
+d174ab98d277d9f5a5611c2c9f419d9f
+MD5 ("123456789012345678901234567890123456789012345678901234567890123456
+78901234567890") = 57edf4a22be3c955ac49da2e2107b67a
+#endif
+
 TUT_UNIT_TEST( "from file" )
 	cout << hash::md5( HBitSourceFile( "./data/karatsuba.bc" ) ) << endl;
 TUT_TEARDOWN()
