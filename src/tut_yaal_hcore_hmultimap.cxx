715856abd29a4548df8fae48e7c464cf95602c72
Marcin Konarski <marcin.konarski@codestation.org>
2009-04-24 17:27:57 +0200

    Tests for HMultiMap.

---
+++ src/tut_yaal_hcore_hmultimap.cxx
@@ -0,0 +1,77 @@
+/*
+---            `tress' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski             ---
+
+	tut_yaal_hcore_hmultimap.cxx - this file is integral part of `tress' project.
+
+	i.  You may not make any changes in Copyright information.
+	ii. You must attach Copyright information to any part of every copy
+	    of this software.
+
+Copyright:
+
+ You are free to use this program as is, you can redistribute binary
+ package freely but:
+  1. You cannot use any part of sources of this software.
+  2. You cannot redistribute any part of sources of this software.
+  3. No reverse engineering is allowed.
+  4. If you want redistribute binary package you cannot demand any fees
+     for this software.
+     You cannot even demand cost of the carrier (CD for example).
+  5. You cannot include it to any commercial enterprise (for example 
+     as a free add-on to payed software or payed newspaper).
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
+*/
+
+#include <TUT/tut.hpp>
+
+#include <yaal/yaal.hxx>
+M_VCSID( "$Id: "__ID__" $" )
+#include "tut_helpers.hxx"
+
+using namespace tut;
+using namespace yaal;
+using namespace yaal::hcore;
+using namespace yaal::hconsole;
+using namespace yaal::tools;
+using namespace yaal::tools::util;
+using namespace tress::tut_helpers;
+
+namespace tut
+{
+
+struct tut_yaal_hcore_hmultimap
+	{
+	virtual ~tut_yaal_hcore_hmultimap( void ) {}
+	typedef HMultiMap<int, int> mm_t;
+	};
+
+TUT_TEST_GROUP_N( tut_yaal_hcore_hmultimap, "yaal::hcore::HMultiMap" );
+
+TUT_UNIT_TEST_N( 1, "find/upper_bound on non existing" )
+	mm_t mm;
+	mm.insert( make_pair( 1, 2 ) );
+	mm.insert( make_pair( 1, 3 ) );
+	mm.insert( make_pair( 3, 4 ) );
+	mm.insert( make_pair( 3, 5 ) );
+	for ( mm_t::const_iterator it = mm.find( 2 ), end = mm.upper_bound( 2 ); it != end; ++ it )
+		fail( "find/upper_bound ranges skewed" );
+TUT_TEARDOWN()
+
+TUT_UNIT_TEST_N( 2, "find/upper_bound on existing" )
+	mm_t mm;
+	mm.insert( make_pair( 1, 2 ) );
+	mm.insert( make_pair( 1, 3 ) );
+	mm.insert( make_pair( 2, 7 ) );
+	mm.insert( make_pair( 2, 8 ) );
+	mm.insert( make_pair( 3, 4 ) );
+	mm.insert( make_pair( 3, 5 ) );
+	int acc = 0;
+	for ( mm_t::const_iterator it = mm.find( 2 ), end = mm.upper_bound( 2 ); it != end; ++ it )
+		acc += (*it).second;
+	ensure_equals( "bad elements selected throu find/upper_bound", acc, 15 );
+TUT_TEARDOWN()
+
+}
+
