97c0832e6d80c21020d31c1646ab2286141708ff
Marcin Konarski <marcin.konarski@codestation.org>
2010-06-16 01:29:13 +0200

    tests for HZipStram added.

---
+++ src/tut_yaal_tools_hzipstream.cxx
@@ -0,0 +1,97 @@
+/*
+---            `tress' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski             ---
+
+	tut_yaal_tools_hzipstream.cxx - this file is integral part of `tress' project.
+
+	i.  You may not make any changes in Copyright information.
+	ii. You must attach Copyright information to any part of every copy
+	    of this software.
+
+Copyright:
+
+ You are free to use this program as is, you can redistribute binary
+ package freely but:
+  1. You cannot use any part of sources of this software.
+  2. You cannot redistribute any part of sources of this software.
+  3. No reverse engineering is allowed.
+  4. If you want redistribute binary package you cannot demand any fees
+     for this software.
+     You cannot even demand cost of the carrier (CD for example).
+  5. You cannot include it to any commercial enterprise (for example 
+     as a free add-on to payed software or payed newspaper).
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
+*/
+
+#include <cstdio>
+#include <TUT/tut.hpp>
+
+#include <yaal/yaal.hxx>
+M_VCSID( "$Id: "__ID__" $" )
+#include "tut_helpers.hxx"
+#include "setup.hxx"
+
+using namespace tut;
+using namespace yaal;
+using namespace yaal::hcore;
+using namespace yaal::hconsole;
+using namespace yaal::tools;
+using namespace yaal::tools::util;
+using namespace tress;
+using namespace tress::tut_helpers;
+
+namespace tut
+{
+
+TUT_SIMPLE_MOCK( tut_yaal_tools_hzipstream );
+TUT_TEST_GROUP_N( tut_yaal_tools_hzipstream, "yaal::tools::HZipStream" );
+
+TUT_UNIT_TEST_N( 1, "compress file" )
+	static char const* const INPUT = ( setup._argc > 1 ) ? setup._argv[ 1 ] : "./data/karatsuba.bc";
+	static char const* const OUTPUT = ( setup._argc > 2 ) ? setup._argv[ 2 ] : "./out/karatsuba.bc.z";
+	HChunk buf( 128 );
+	HFile in;
+	HFile out;
+	if ( HString( "-" ) == INPUT )
+		in.open( stdin );
+	else
+		in.open( INPUT, HFile::OPEN::READING );
+	if ( HString( "-" ) == OUTPUT )
+		out.open( stdout );
+	else
+		out.open( OUTPUT, HFile::OPEN::WRITING );
+	HZipStream z( out, HZipStream::MODE::DEFLATE );
+	int long nRead( 0 );
+	while ( ( nRead = in.read( buf.raw(), buf.get_size() ) ) > 0 )
+		z.write( buf.raw(), nRead );
+	if ( setup._argc < 1 )
+		{
+		}
+TUT_TEARDOWN()
+
+TUT_UNIT_TEST_N( 2, "decompress file" )
+	static char const* const INPUT = ( setup._argc > 1 ) ? setup._argv[ 1 ] : "./out/karatsuba.bc.z";
+	static char const* const OUTPUT = ( setup._argc > 2 ) ? setup._argv[ 2 ] : "./out/karatsuba.bc";
+	HChunk buf( 128 );
+	HFile in;
+	HFile out;
+	if ( HString( "-" ) == INPUT )
+		in.open( stdin );
+	else
+		in.open( INPUT, HFile::OPEN::READING );
+	if ( HString( "-" ) == OUTPUT )
+		out.open( stdout );
+	else
+		out.open( OUTPUT, HFile::OPEN::WRITING );
+	HZipStream z( in, HZipStream::MODE::INFLATE );
+	int long nRead( 0 );
+	while ( ( nRead = z.read( buf.raw(), buf.get_size() ) ) > 0 )
+		out.write( buf.raw(), nRead );
+	if ( setup._argc < 1 )
+		{
+		}
+TUT_TEARDOWN()
+
+}
+
