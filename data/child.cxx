fcd65dc8ec11e78aae505712debfc2ae8909a796
Marcin Konarski <marcin.konarski@codestation.org>
2010-10-02 12:40:22 +0200

    child-simple error handlin added.

--- data/child.cxx
+++ data/child.cxx
@@ -23,23 +23,36 @@ int main( int, char** )
 #ifdef __MSVCXX__
 	/* DebugBreak(); */
 #endif
-	ofstream log( ".\\out\\hpipedchild.log" );
-	log << now() << ": child start ..." << endl;
-	string line;
-	cin >> line;
-	log << now() << ": read input: " << line << endl;
-	string output;
-	if ( line == "out" )
-		cout << ( output = "hello-OUT" ) << endl;
-	else if ( line == "err" )
-		cerr << ( output = "hello-ERR" ) << endl;
-	else
+	int ret( 0 );
+	try
 		{
-		cout << ( output = "error" ) << endl;
-		cerr << output << endl;
+		string const path( "./out/hpipedchild.log" );
+		ofstream log( path.c_str() );
+		if ( ! log )
+			throw runtime_error( "Cannot open file: " + path );
+
+		log << now() << ": child start ..." << endl;
+		string line;
+		cin >> line;
+		log << now() << ": read input: " << line << endl;
+		string output;
+		if ( line == "out" )
+			cout << ( output = "hello-OUT" ) << endl;
+		else if ( line == "err" )
+			cerr << ( output = "hello-ERR" ) << endl;
+		else
+			{
+			cout << ( output = "error" ) << endl;
+			cerr << output << endl;
+			}
+		log << now() << ": written output [" << output << "]" << endl;
+		log << now() << ": exiting" << endl;
 		}
-	log << now() << ": written output [" << output << "]" << endl;
-	log << now() << ": exiting" << endl;
-	return ( 0 );
+	catch ( exception const& e )
+		{
+		cerr << "Exception caught: " << e.what() << endl;
+		ret = 1;
+		}
+	return ( ret );
 	}
 
