39bdcba25303f99b7a48c3ff6ae72cc9aa748183
Marcin Konarski <marcin.konarski@codestation.org>
2011-11-20 23:13:39 +0100

    Further parser work.

--- src/tut_yaal_tools_hllexecutingparser.cxx
+++ src/tut_yaal_tools_hllexecutingparser.cxx
@@ -41,79 +41,125 @@ namespace ll {
 
 class HRule {
 public:
+	typedef HRule this_type;
 	typedef HBoundCall<> action_t;
 	typedef HBoundCall<> executor_t;
 	typedef HArray<executor_t> executors_t;
-	typedef HRule this_type;
 	typedef yaal::hcore::HPointer<HRule> ptr_t;
 protected:
 	ptr_t _rule;
 	action_t _action;
 	executors_t _excutors;
+	bool _matched;
 public:
 	HRule( void )
-		: _rule(), _action(), _excutors()
+		: _rule(), _action(), _excutors(), _matched( false )
 		{}
 public:
 	HRule( HRule const& rule_ )
-		: _rule( rule_.clone() ), _action(), _excutors()
+		: _rule( rule_.clone() ), _action(), _excutors(), _matched( false )
 		{}
 	virtual ~HRule( void )
 		{}
-	HRule operator[]( action_t action_ )
-		{ return ( HRule( _rule, action_ ) ); }
-	bool operator()( HString const& input_ )
-		{ return ( parse( input_.begin(), input_.end() ) != input_.end() ); }
-	bool operator()( HString::const_iterator first_, HString::const_iterator last_ )
-		{ return ( parse( first_, last_ ) != last_ ); }
-	void operator()( void )
-		{ execute(); }
+	HRule operator[]( action_t action_ ) {
+		M_PROLOG
+		return ( HRule( _rule, action_ ) );
+		M_EPILOG
+	}
+	bool operator()( HString const& input_ ) {
+		M_PROLOG
+		return ( parse( input_.begin(), input_.end() ) != input_.end() );
+		M_EPILOG
+	}
+	bool operator()( HString::const_iterator first_, HString::const_iterator last_ ) {
+		M_PROLOG
+		return ( parse( first_, last_ ) != last_ );
+		M_EPILOG
+	}
+	void operator()( void ) {
+		M_PROLOG
+		execute();
+		return;
+		M_EPILOG
+	}
 	void execute( void ) {
+		M_PROLOG
+		M_ENSURE( _matched );
 		do_execute();
 		for ( executors_t::iterator it( _excutors.begin() ), end( _excutors.end() ); it != end; ++ it )
 			(*it)();
+		return;
+		M_EPILOG
 	}
 	void cancel_execution( void ) {
+		M_PROLOG
+		_matched = false;
 		do_cancel_execution();
 		_excutors.clear();
+		return;
+		M_EPILOG
 	}
-	HString::const_iterator parse( HString::const_iterator first_, HString::const_iterator last_ )
-		{ return ( do_parse( first_, last_ ) ); }
+	HString::const_iterator parse( HString::const_iterator first_, HString::const_iterator last_ ) {
+		M_PROLOG
+		HString::const_iterator it( do_parse( first_, last_ ) );
+		_matched = it != first_;
+		return ( it );
+		M_EPILOG
+	}
 	ptr_t clone( void ) const {
+		M_PROLOG
 		return ( do_clone() );
+		M_EPILOG
 	}
-	bool is_optional( void ) const
-		{ return ( do_is_optional() ); }
+	bool is_optional( void ) const {
+		M_PROLOG
+		return ( do_is_optional() );
+		M_EPILOG
+	}
 private:
 	HRule( ptr_t rule_, action_t action_ )
-		: _rule( rule_ ), _action( action_ ), _excutors()
+		: _rule( rule_ ), _action( action_ ), _excutors(), _matched( false )
 		{}
 protected:
 	virtual HString::const_iterator do_parse( HString::const_iterator first_, HString::const_iterator last_ ) {
+		M_PROLOG
 		HString::const_iterator ret( !! _rule ? _rule->parse( first_, last_ ) : first_ );
-		if ( ret == last_ )
+		if ( ( ret == last_ ) && !! _action )
 			_excutors.push_back( _action );
 		return ( ret );
+		M_EPILOG
 	}
 	virtual void do_execute( void ) {
+		M_PROLOG
 		if ( !! _rule )
 			_rule->execute();
+		return;
+		M_EPILOG
 	}
 	virtual void do_cancel_execution( void ) {
+		M_PROLOG
 		if ( !! _rule )
 			_rule->cancel_execution();
+		return;
+		M_EPILOG
 	}
 	virtual ptr_t do_clone( void ) const {
+		M_PROLOG
 		return ( !! _rule ? _rule->clone() : ptr_t() );
+		M_EPILOG
 	}
 	virtual bool do_is_optional( void ) const
 		{ return ( false ); }
 	static HString::const_iterator skip_space( HString::const_iterator first_, HString::const_iterator last_ ) {
+		M_PROLOG
 		while ( ( first_ != last_ ) && isspace( *first_ ) )
 			++ first_;
 		return ( first_ );
+		M_EPILOG
 	}
 };
+
+typedef HExceptionT<HRule> HRuleException;
 
 class HFollows : public HRule {
 	typedef yaal::hcore::HList<HRule::ptr_t> rules_t;
@@ -123,27 +169,39 @@ public:
 	typedef HRule base_type;
 	HFollows( HRule const& predecessor_, HRule const& successor_ )
 		: HRule(), _rules() {
+		M_PROLOG
 		_rules.push_back( predecessor_.clone() );
 		_rules.push_back( successor_.clone() );
+		return;
+		M_EPILOG
 	}
 	HFollows( HFollows const& follows_ )
 		: HRule(), _rules() {
+		M_PROLOG
 		for ( rules_t::const_iterator it( follows_._rules.begin() ), end( follows_._rules.end() ); it != end; ++ it )
 			_rules.push_back( (*it)->clone() );
+		return;
+		M_EPILOG
 	}
 	HFollows( HFollows const& predecessors_, HRule const& successor_ )
 		: HRule(), _rules() {
+		M_PROLOG
 		for ( rules_t::const_iterator it( predecessors_._rules.begin() ), end( predecessors_._rules.end() ); it != end; ++ it )
 			_rules.push_back( (*it)->clone() );
 		_rules.push_back( successor_.clone() );
+		return;
+		M_EPILOG
 	}
 	virtual ~HFollows( void )
 		{}
 protected:
-	ptr_t do_clone( void ) const {
+	virtual ptr_t do_clone( void ) const {
+		M_PROLOG
 		return ( ptr_t( new HFollows( *this ) ) );
+		M_EPILOG
 	}
 	virtual HString::const_iterator do_parse( HString::const_iterator first_, HString::const_iterator last_ ) {
+		M_PROLOG
 		HString::const_iterator orig( first_ );
 		bool matched( true );
 		for ( rules_t::iterator it( _rules.begin() ), end( _rules.end() ); it != end; ++ it ) {
@@ -155,18 +213,25 @@ protected:
 			}
 		}
 		return ( matched ? first_ : orig );
+		M_EPILOG
 	}
-	void do_cancel_execution( void ) {
+	virtual void do_cancel_execution( void ) {
+		M_PROLOG
 		for ( rules_t::iterator it( _rules.begin() ), end( _rules.end() ); it != end; ++ it )
 			(*it)->cancel_execution();
 		return;
+		M_EPILOG
 	}
-	void do_execute( void ) {
+	virtual void do_execute( void ) {
+		M_PROLOG
 		for ( rules_t::iterator it( _rules.begin() ), end( _rules.end() ); it != end; ++ it )
 			(*it)->execute();
 		return;
+		M_EPILOG
 	}
 };
+
+typedef HExceptionT<HFollows, HRuleException> HFollowsException;
 
 class HKleeneStar : public HRule {
 public:
@@ -181,18 +246,24 @@ public:
 	virtual ~HKleeneStar( void )
 		{}
 protected:
-	ptr_t do_clone( void ) const {
+	virtual ptr_t do_clone( void ) const {
+		M_PROLOG
 		return ( ptr_t( new HKleeneStar( *this ) ) );
+		M_EPILOG
 	}
-	bool do_is_optional( void ) const
+	virtual bool do_is_optional( void ) const
 		{ return ( true ); }
 	virtual HString::const_iterator do_parse( HString::const_iterator first_, HString::const_iterator last_ ) {
+		M_PROLOG
 		HString::const_iterator old( last_ );
 		while ( ( first_ != last_ ) && ( first_ != old ) )
 			first_ = HRule::do_parse( old = first_, last_ );
 		return ( first_ );
+		M_EPILOG
 	}
 };
+
+typedef HExceptionT<HKleeneStar, HRuleException> HKleeneStarException;
 
 class HKleenePlus : public HRule {
 public:
@@ -207,31 +278,106 @@ public:
 	virtual ~HKleenePlus( void )
 		{}
 protected:
-	ptr_t do_clone( void ) const {
+	virtual ptr_t do_clone( void ) const {
+		M_PROLOG
 		return ( ptr_t( new HKleenePlus( *this ) ) );
+		M_EPILOG
 	}
 	virtual HString::const_iterator do_parse( HString::const_iterator first_, HString::const_iterator last_ ) {
+		M_PROLOG
 		HString::const_iterator old( last_ );
 		while ( ( first_ != last_ ) && ( first_ != old ) )
 			first_ = HRule::do_parse( old = first_, last_ );
 		return ( first_ );
+		M_EPILOG
 	}
 };
 
+typedef HExceptionT<HKleenePlus, HRuleException> HKleenePlusException;
+
+class HAlternative : public HRule {
+	typedef yaal::hcore::HList<HRule::ptr_t> rules_t;
+	rules_t _rules;
+public:
+	typedef HAlternative this_type;
+	typedef HRule base_type;
+	HAlternative( HRule const& choice1_, HRule const& choice2_ )
+		: HRule(), _rules() {
+		M_PROLOG
+		_rules.push_back( choice1_.clone() );
+		_rules.push_back( choice2_.clone() );
+		return;
+		M_EPILOG
+	}
+	HAlternative( HAlternative const& alternative_ )
+		: HRule(), _rules() {
+		M_PROLOG
+		for ( rules_t::const_iterator it( alternative_._rules.begin() ), end( alternative_._rules.end() ); it != end; ++ it )
+			_rules.push_back( (*it)->clone() );
+		return;
+		M_EPILOG
+	}
+	HAlternative( HAlternative const& alternative_, HRule const& choice_ )
+		: HRule(), _rules() {
+		M_PROLOG
+		for ( rules_t::const_iterator it( alternative_._rules.begin() ), end( alternative_._rules.end() ); it != end; ++ it )
+			_rules.push_back( (*it)->clone() );
+		_rules.push_back( choice_.clone() );
+		return;
+		M_EPILOG
+	}
+protected:
+	virtual ptr_t do_clone( void ) const {
+		M_PROLOG
+		return ( ptr_t( new HAlternative( *this ) ) );
+		M_EPILOG
+	}
+};
+
+typedef HExceptionT<HAlternative, HRuleException> HAlternativeException;
+
+class HOptional : public HRule {
+protected:
+	virtual bool do_is_optional( void ) const
+		{ return ( true ); }
+};
+
+typedef HExceptionT<HOptional, HRuleException> HOptionalException;
+
 HFollows operator >> ( HRule const& predecessor_, HRule const& successor_ ) {
+	M_PROLOG
 	return ( HFollows( predecessor_, successor_ ) );
+	M_EPILOG
 }
 
 HFollows operator >> ( HFollows const& predecessors_, HRule const& successor_ ) {
+	M_PROLOG
 	return ( HFollows( predecessors_, successor_ ) );
+	M_EPILOG
+}
+
+HAlternative operator | ( HRule const& choice1_, HRule const& choice2_ ) {
+	M_PROLOG
+	return ( HAlternative( choice1_, choice2_ ) );
+	M_EPILOG
+}
+
+HAlternative operator | ( HAlternative const& alternative_, HRule const& choice_ ) {
+	M_PROLOG
+	return ( HAlternative( alternative_, choice_ ) );
+	M_EPILOG
 }
 
 HKleeneStar operator* ( HRule const& rule_ ) {
+	M_PROLOG
 	return ( HKleeneStar( rule_ ) );
+	M_EPILOG
 }
 
 HKleenePlus operator+ ( HRule const& rule_ ) {
+	M_PROLOG
 	return ( HKleenePlus( rule_ ) );
+	M_EPILOG
 }
 
 class HReal : public HRule {
@@ -268,18 +414,27 @@ public:
 		_actionNumber( real_._actionNumber ), _actionString( real_._actionString ), _cache( real_._cache ) {}
 	using HRule::operator[];
 	HReal operator[]( action_double_t const& action_ ) {
+		M_PROLOG
 		return ( HReal( action_, action_double_long_t(), action_number_t(), action_string_t() ) );
+		M_EPILOG
 	}
 	HReal operator[]( action_double_long_t const& action_ ) {
+		M_PROLOG
 		return ( HReal( action_double_t(), action_, action_number_t(), action_string_t() ) );
+		M_EPILOG
 	}
 	HReal operator[]( action_number_t const& action_ ) {
+		M_PROLOG
 		return ( HReal( action_double_t(), action_double_long_t(), action_, action_string_t() ) );
+		M_EPILOG
 	}
 	HReal operator[]( action_string_t const& action_ ) {
+		M_PROLOG
 		return ( HReal( action_double_t(), action_double_long_t(), action_number_t(), action_ ) );
+		M_EPILOG
 	}
 	virtual HString::const_iterator do_parse( HString::const_iterator first_, HString::const_iterator last_ ) {
+		M_PROLOG
 		HString::const_iterator scan( skip_space( first_, last_ ) );
 		_cache.clear();
 		real_paring_state_t state( START );
@@ -337,12 +492,17 @@ public:
 			}
 		}
 		return ( first_ );
+		M_EPILOG
 	}
 protected:
-	ptr_t do_clone( void ) const {
+	virtual ptr_t do_clone( void ) const {
+		M_PROLOG
 		return ( ptr_t( new HReal( *this ) ) );
+		M_EPILOG
 	}
 } real;
+
+typedef HExceptionT<HReal, HRuleException> HRealException;
 
 class HInteger : public HRule {
 	typedef HBoundCall<void ( int long const& )> action_int_long_t;
@@ -376,18 +536,27 @@ public:
 		_actionNumber( real_._actionNumber ), _actionString( real_._actionString ), _cache( real_._cache ) {}
 	using HRule::operator[];
 	HInteger operator[]( action_int_long_t const& action_ ) {
+		M_PROLOG
 		return ( HInteger( action_, action_int_t(), action_number_t(), action_string_t() ) );
+		M_EPILOG
 	}
 	HInteger operator[]( action_int_t const& action_ ) {
+		M_PROLOG
 		return ( HInteger( action_int_long_t(), action_, action_number_t(), action_string_t() ) );
+		M_EPILOG
 	}
 	HInteger operator[]( action_number_t const& action_ ) {
+		M_PROLOG
 		return ( HInteger( action_int_long_t(), action_int_t(), action_, action_string_t() ) );
+		M_EPILOG
 	}
 	HInteger operator[]( action_string_t const& action_ ) {
+		M_PROLOG
 		return ( HInteger( action_int_long_t(), action_int_t(), action_number_t(), action_ ) );
+		M_EPILOG
 	}
 	virtual HString::const_iterator do_parse( HString::const_iterator first_, HString::const_iterator last_ ) {
+		M_PROLOG
 		HString::const_iterator scan( skip_space( first_, last_ ) );
 		_cache.clear();
 		real_paring_state_t state( START );
@@ -434,12 +603,17 @@ public:
 			}
 		}
 		return ( first_ );
+		M_EPILOG
 	}
 protected:
-	ptr_t do_clone( void ) const {
+	virtual ptr_t do_clone( void ) const {
+		M_PROLOG
 		return ( ptr_t( new HInteger( *this ) ) );
+		M_EPILOG
 	}
 } integer;
+
+typedef HExceptionT<HInteger, HRuleException> HIntegerException;
 
 class HCharacter : public HRule {
 	typedef HBoundCall<void ( char const& )> action_t;
@@ -460,12 +634,17 @@ public:
 		{}
 	using HRule::operator[];
 	HCharacter operator[]( action_t const& action_ ) {
+		M_PROLOG
 		return ( HCharacter( _character, action_ ) );
+		M_EPILOG
 	}
 	HCharacter operator() ( char character_ ) {
+		M_PROLOG
 		return ( HCharacter( character_, _action ) );
+		M_EPILOG
 	}
 	virtual HString::const_iterator do_parse( HString::const_iterator first_, HString::const_iterator last_ ) {
+		M_PROLOG
 		M_ENSURE( first_ != last_ );
 		HString::const_iterator scan( skip_space( first_, last_ ) );
 		char c( *scan );
@@ -478,20 +657,29 @@ public:
 			}
 		}
 		return ( first_ );
+		M_EPILOG
 	}
 protected:
-	ptr_t do_clone( void ) const {
+	virtual ptr_t do_clone( void ) const {
+		M_PROLOG
 		return ( ptr_t( new HCharacter( *this ) ) );
+		M_EPILOG
 	}
 } character;
 
+typedef HExceptionT<HCharacter, HRuleException> HCharacterException;
+
 HFollows operator >> ( char character_, HRule const& successor_ ) {
+	M_PROLOG
 	return ( HFollows( HCharacter( character_ ), successor_  ) );
+	M_EPILOG
 }
 
 template<typename container_t>
 HBoundCall<void ( typename container_t::value_type const& )> push_back( container_t& container ) {
+	M_PROLOG
 	return ( call( &container_t::push_back, &container, _1 ) );
+	M_EPILOG
 }
 
 }
@@ -514,12 +702,25 @@ namespace tut {
 TUT_SIMPLE_MOCK( tut_yaal_tools_hllexecutingparser );
 TUT_TEST_GROUP( tut_yaal_tools_hllexecutingparser, "yaal::tools::HLLExecutingParser" );
 
-TUT_UNIT_TEST( 1, "the test" )
+TUT_UNIT_TEST( 1, "empty parser" )
+	HRule r;
+	HString text( "1+2" );
+	ENSURE( "empty parser consumed input", r.parse( text.begin(), text.end() ) == text.begin() );
+	ENSURE( "empty parser succeeded on non empty input", r( text ) );
+	try {
+		r();
+		FAIL( "execution of empty parser succeeded" );
+	} catch ( HRuleException const& ) {
+		// ok
+	}
+TUT_TEARDOWN()
+
+TUT_UNIT_TEST( 50, "the test" )
 	HArray<double> v;
 	if ( real[push_back(v)]( "3.141592653589793" ) )
 		cout << "1: failed to consume input" << endl;
 	HRule r( real[push_back(v)] >> *( ',' >> real[push_back(v)] ) );
-	r( "3.141592653589793, -2.718281828459045, 17, kupa" );
+	r( "3.141592653589793, -2.718281828459045, 17" );
 	r();
 	if ( r( "3.141592653589793, -2.718281828459045, 17, kupa" ) )
 		cout << "2: failed to consume input" << endl;
