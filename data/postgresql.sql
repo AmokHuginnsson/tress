ca69cffa712950e46810800c1e7f06a07c628947
Marcin Konarski <marcin.konarski@codestation.org>
2011-11-30 13:19:07 +0100

    setting sequence number for config table

--- data/postgresql.sql
+++ data/postgresql.sql
@@ -17,6 +17,8 @@ INSERT INTO config ( id, name, data ) VALUES( 1, 'one', '1' );
 INSERT INTO config ( id, name, data ) VALUES( 2, 'two', '22' );
 INSERT INTO config ( id, name, data ) VALUES( 3, 'three', '333' );
 INSERT INTO config ( id, name, data ) VALUES( 4, 'four', '4444' );
+SELECT setval( 'config_id_seq', 4 );
 
 GRANT SELECT,INSERT,UPDATE,DELETE ON config TO tress;
 GRANT SELECT,UPDATE ON config_id_seq TO tress;
+
