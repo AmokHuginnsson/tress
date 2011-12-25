587ace34b54c23c0c7d6f5e64c75d8d832dd5484
Marcin Konarski <marcin.konarski@codestation.org>
2011-12-25 05:32:05 +0100

    more tests for threading subsystem

--- src/tut_yaal_hcore_htls.cxx
+++ src/tut_yaal_hcore_htls.cxx
@@ -27,6 +27,7 @@ Copyright:
 #include <TUT/tut.hpp>
 
 #include <yaal/hcore/htls.hxx>
+#include <yaal/tools/util.hxx>
 M_VCSID( "$Id: "__ID__" $" )
 #include "tut_helpers.hxx"
 
@@ -50,17 +51,68 @@ public:
 };
 
 struct tut_yaal_hcore_htls : public simple_mock<tut_yaal_hcore_htls> {
-	virtual ~tut_yaal_hcore_htls( void ) {}
 	typedef HTLS<TLSTest> tls_t;
+	HMutex _mutex;
+	HCondition _cond;
+	HEvent _allOn;
+	int _instances;
+	tut_yaal_hcore_htls( void )
+		: _mutex( HMutex::TYPE::RECURSIVE ), _cond( _mutex ), _allOn(), _instances( 0 )
+		{}
+	virtual ~tut_yaal_hcore_htls( void ) { }
+	void* run( tls_t& tls_ ) {
+		M_PROLOG
+		HThread::set_name( "tut::HThread" );
+		HLock l( _mutex );
+		++ _instances;
+		tls_->foo();
+		if ( _instances == 3 ) {
+			clog << "all on" << endl;
+			_allOn.signal();
+		}
+		_cond.wait( 10, 0 );
+		clog << "leaving ..." << endl;
+		return ( NULL );
+		M_EPILOG
+	}
+	void schedule_stop( void ) {
+		M_PROLOG
+		HLock l( _mutex );
+		_cond.broadcast();
+		return;
+		M_EPILOG
+	}
+	void wait_for_all( void ) {
+		M_PROLOG
+		_allOn.wait();
+		return;
+		M_EPILOG
+	}
 };
 TUT_TEST_GROUP( tut_yaal_hcore_htls, "yaal::hcore::HTLS" );
 
-TUT_UNIT_TEST( 1, "the test" )
+TUT_UNIT_TEST( 1, "tls in main thread" )
 	tls_t tls;
 	tls->foo();
 	external_lock_t l( tls.acquire() );
 	tls_t::iterator it( tls.begin() );
-	ENSURE( "inconsitient state", tls.operator->() == it->get() );
+	ENSURE( "inconsitient state", tls.operator->() == it->first );
+TUT_TEARDOWN()
+
+TUT_UNIT_TEST( 2, "multiple instances" )
+	tls_t tls;
+	tls->foo();
+	HThread t1;
+	HThread t2;
+	HThread t3;
+	TUT_INVOKE( t1.spawn( call( &tut_yaal_hcore_htls::run, this, ref( tls ) ) ); );
+	t2.spawn( call( &tut_yaal_hcore_htls::run, this, ref( tls ) ) );
+	t3.spawn( call( &tut_yaal_hcore_htls::run, this, ref( tls ) ) );
+	TUT_INVOKE( wait_for_all(); );
+	external_lock_t l( tls.acquire() );
+	tls_t::iterator it( tls.begin() );
+	ENSURE_EQUALS( "bad instance count", tls.get_instance_count(), 4 );
+	TUT_INVOKE( schedule_stop(); );
 TUT_TEARDOWN()
 
 }
