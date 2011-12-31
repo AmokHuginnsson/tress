2c5706ebfa814fefb47c70480847015e53064ec0
Marcin Konarski <marcin.konarski@codestation.org>
2011-12-31 20:28:10 +0100

    Added tests for getting schema information from databases.

--- src/tut_yaal_dbwrapper_hrecordset.cxx
+++ src/tut_yaal_dbwrapper_hrecordset.cxx
@@ -209,5 +209,55 @@ TUT_UNIT_TEST( 12, "Oracle engine" )
 TUT_TEARDOWN()
 #endif /* defined( HAVE_OCI_H ) */
 
+void test_schema( HDataBase::ptr_t db_ ) {
+	M_PROLOG
+	HDataBase::table_list_t tl( db_->get_tables() );
+	ENSURE_EQUALS( "bad table list size", tl.get_size(), 1 );
+	HDataBase::table_list_t tlExpect;
+	tlExpect.push_back( "config" );
+	ENSURE_EQUALS( "bad table list contents", tl, tlExpect );
+	HDataBase::column_list_t cl( db_->get_columns( tl[0] ) );
+	ENSURE_EQUALS( "bad column list count", cl.get_size(), 3 );
+	HDataBase::column_list_t clExpect;
+	clExpect.push_back( "id" );
+	clExpect.push_back( "name" );
+	clExpect.push_back( "data" );
+	ENSURE_EQUALS( "bad column list contents", cl, clExpect );
+	return;
+	M_EPILOG
+}
+
+#if defined( HAVE_SQLITE3_H )
+TUT_UNIT_TEST( 13, "sqlite3 schema" )
+	HDataBase::ptr_t db = HDataBase::get_connector( ODBConnector::DRIVER::SQLITE3 );
+	db->connect( "./out/tress", "", "" );
+	test_schema( db );
+TUT_TEARDOWN()
+#endif /* defined( HAVE_SQLITE3_H ) */
+
+#if defined( HAVE_POSTGRESQL_LIBPQ_FE_H ) || defined( HAVE_LIBPQ_FE_H )
+	HDataBase::ptr_t db = HDataBase::get_connector( ODBConnector::DRIVER::POSTGRESQL );
+	db->connect( "tress", "tress", "tr3ss" );
+	test_schema( db );
+#endif /* defined( HAVE_POSTGRESQL_LIBPQ_FE_H ) || defined( HAVE_LIBPQ_FE_H ) */
+
+#if defined( HAVE_MYSQL_MYSQL_H )
+	HDataBase::ptr_t db = HDataBase::get_connector( ODBConnector::DRIVER::MYSQL );
+	db->connect( "tress", "tress", "tr3ss" );
+	test_schema( db );
+#endif /* defined( HAVE_MYSQL_MYSQL_H ) */
+
+#if defined( HAVE_IBASE_H )
+	HDataBase::ptr_t db = HDataBase::get_connector( ODBConnector::DRIVER::FIREBIRD );
+	db->connect( "tress", "tress", "tr3ss" );
+	test_schema( db );
+#endif /* defined( HAVE_IBASE_H ) */
+
+#if 0 && defined( HAVE_OCI_H )
+	HDataBase::ptr_t db = HDataBase::get_connector( ODBConnector::DRIVER::ORACLE );
+	db->connect( "tress", "tress", "tr3ss" );
+	test_schema( db );
+#endif /* defined( HAVE_OCI_H ) */
+
 }
 
