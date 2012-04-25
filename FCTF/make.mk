2754edba3f4455ebae5707341d7d4e17ea122743
Marcin Konarski <marcin.konarski@codestation.org>
2012-04-25 23:56:39 +0200

    Fixed FCTC.

--- FCTF/make.mk
+++ FCTF/make.mk
@@ -8,12 +8,12 @@ AOBJS = $(patsubst %.$(ASS),%.$(AOS),$(ASRCS))
 DIRS += $(DIR_BUILD)/must_not_compile
 
 %.$(AOS): %.$(ASS)
-	@printf "%b" "Anti-Compiling \`$(subst $(DIR_ROOT)/,,$(<))' ... "; \
+	@$(eval $(call IDENT_HELPER,$(<))) $(call msg,printf "%b" "$($(CURR_PROGRESS_INDICATOR))$(eval $(call PROGRESS_INDICATOR))Anti-Compiling \`$(subst $(DIR_ROOT)/,,$(<))' ... " && ) \
 	mkdir -p $(DIR_BUILD)/must_not_compile ; \
-	/bin/rm -f "$(@)"; \
-	$(DXX) $(CXXFLAGS) -xc++ -DBUG_0 -MM $(<) -MT $(@) -MT $(@:.$(OS)=.$(DS)) -MF $(@:.$(OS)=.$(DS)) && \
-	$(ACXX) $(CXXFLAGS) $(<) -c -o $(@) 2>&1 | tee -a make.log && \
-	test -f $(@) && \
-	printf "%b$(NL)" "done.$(CL)"
+	/bin/rm -f "$(@)" && \
+	$(call invoke,$(DXX) $(CXXFLAGS) -xc++ -DMAKE_DEP -MM $(<) -MT $(@) -MT $(@:.$(OS)=.$(DS)) -MF $(@:.$(OS)=.$(DS))) && \
+	$(call invoke,$(ACXX) $(CXXFLAGS) $(<) -c -o $(@) 2>&1 | tee -a make.log) && \
+	test -f $(@) \
+	$(call msg,&& printf "%b$(NL)" "done.$(CL)")
 
 EXTRA_DEPS_TARGET_tress=$(AOBJS)
