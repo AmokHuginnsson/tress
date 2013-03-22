faf2a12d4185e07d77e9b21416fe84644485d8cc
Marcin Konarski <marcin.konarski@codestation.org>
2013-03-22 04:06:19 +0100

    Cleanup headers.

--- src/tut.cxx
+++ src/tut.cxx
@@ -26,17 +26,13 @@ Copyright:
 
 #include <TUT/tut.hpp>
 
-#include <yaal/yaal.hxx>
 M_VCSID( "$Id: "__ID__" $" )
 #include "tut_helpers.hxx"
 
 using namespace tut;
-using namespace std;
 using namespace yaal;
 using namespace yaal::hcore;
-using namespace yaal::hconsole;
 using namespace yaal::tools;
-using namespace yaal::tools::util;
 using namespace tress::tut_helpers;
 
 namespace tut {
