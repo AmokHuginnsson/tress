f39352f59a96a26decea52e83165ad01f87ed977
Marcin Konarski <marcin.konarski@codestation.org>
2015-01-25 23:37:51 +0100

    HHuginn: Added tests for reporting errors during execution.

--- src/tut_yaal_tools_hhuginn.cxx
+++ src/tut_yaal_tools_hhuginn.cxx
@@ -63,6 +63,7 @@ struct tut_yaal_tools_hhuginn : public simple_mock<tut_yaal_tools_hhuginn> {
 	void test_preprocessing( prog_src_t, prog_src_t, int );
 	void test_parse( prog_src_t, int const[3], int );
 	void test_compile( prog_src_t, int const[3], int );
+	void test_execute( prog_src_t, int const[3], int );
 	void test_file( hcore::HString const& );
 	hcore::HString const& execute( hcore::HString const& );
 };
@@ -597,84 +598,84 @@ char const progCompileErr0[] =
 	"}\n"
 ;
 
-char const progCompileErr00[] =
+char const progCompileErr1[] =
 	"main() {\n"
 	"\tx = 1 - 1.5;\n"
 	"\treturn ( 0 );\n"
 	"}\n"
 ;
 
-char const progCompileErr1[] =
+char const progCompileErr2[] =
 	"main() {\n"
 	"\tx = 1 * 1.5;\n"
 	"\treturn ( 0 );\n"
 	"}\n"
 ;
 
-char const progCompileErr10[] =
+char const progCompileErr3[] =
 	"main() {\n"
 	"\tx = 1 / 1.5;\n"
 	"\treturn ( 0 );\n"
 	"}\n"
 ;
 
-char const progCompileErr11[] =
+char const progCompileErr4[] =
 	"main() {\n"
 	"\tx = 1 % 1.5;\n"
 	"\treturn ( 0 );\n"
 	"}\n"
 ;
 
-char const progCompileErr2[] =
+char const progCompileErr5[] =
 	"main() {\n"
 	"\tx = 1 ^ 1.5;\n"
 	"\treturn ( 0 );\n"
 	"}\n"
 ;
 
-char const progCompileErr3[] =
+char const progCompileErr6[] =
 	"main() {\n"
 	"\tx = 2 ^ 2 ^ 2.5;\n"
 	"\treturn ( 0 );\n"
 	"}\n"
 ;
 
-char const progCompileErr4[] =
+char const progCompileErr7[] =
 	"main() {\n"
 	"\tx = 2 ^ 2.5 ^ 2;\n"
 	"\treturn ( 0 );\n"
 	"}\n"
 ;
 
-char const progCompileErr5[] =
+char const progCompileErr8[] =
 	"main() {\n"
 	"\tx = 2.5 ^ 2 ^ 2;\n"
 	"\treturn ( 0 );\n"
 	"}\n"
 ;
 
-char const progCompileErr6[] =
+char const progCompileErr9[] =
 	"main() {\n"
 	"\tx = 1 == 1.5;\n"
 	"\treturn ( 0 );\n"
 	"}\n"
 ;
 
-char const progCompileErr7[] =
+char const progCompileErr10[] =
 	"main() {\n"
 	"\tx = 1 != 1.5;\n"
 	"\treturn ( 0 );\n"
 	"}\n"
 ;
 
-char const progCompileErr8[] =
+char const progCompileErr11[] =
 	"main() {\n"
 	"\tx = 1 < 1.5;\n"
 	"\treturn ( 0 );\n"
 	"}\n"
 ;
 
-char const progCompileErr9[] =
+char const progCompileErr12[] =
 	"main() {\n"
 	"\tx = 1 > 1.5;\n"
 	"\treturn ( 0 );\n"
@@ -698,38 +699,180 @@ void tut_yaal_tools_hhuginn::test_compile( prog_src_t prog_, int const err_[3], 
 TUT_UNIT_TEST( "report compilation error" )
 	prog_src_t progCompileErr[] = {
 		progCompileErr0,
-		progCompileErr00,
 		progCompileErr1,
-		progCompileErr10,
-		progCompileErr11,
 		progCompileErr2,
 		progCompileErr3,
 		progCompileErr4,
 		progCompileErr5,
 		progCompileErr6,
 		progCompileErr7,
+		progCompileErr8,
+		progCompileErr9,
+		progCompileErr10,
+		progCompileErr11,
+		progCompileErr12,
 		NULL
 	};
 	int const err[][3] = {
 		{ 16, 2, 8 },   // 0
-		{ 16, 2, 8 },   // 00
 		{ 16, 2, 8 },   // 1
+		{ 16, 2, 8 },   // 2
+		{ 16, 2, 8 },   // 3
+		{ 16, 2, 8 },   // 4
+		{ 16, 2, 8 },   // 5
+		{ 20, 2, 12 },  // 6
+		{ 22, 2, 14 },  // 7
+		{ 18, 2, 10 },  // 8
+		{ 16, 2, 8 },   // 9
 		{ 16, 2, 8 },   // 10
 		{ 16, 2, 8 },   // 11
-		{ 16, 2, 8 },   // 2
-		{ 20, 2, 12 },  // 3
-		{ 22, 2, 14 },  // 4
-		{ 18, 2, 10 },  // 5
-		{ 16, 2, 8 },   // 6
-		{ 16, 2, 8 },   // 7
-		{ 16, 2, 8 },   // 8
-		{ 16, 2, 8 },   // 9
+		{ 16, 2, 8 },   // 12
 		{ 0, 0, 0 }
 	};
 	int const (*e)[3]( err );
 	for ( prog_src_t* prog( begin( progCompileErr ) ), * progEnd( end( progCompileErr ) ); prog != progEnd; ++ prog, ++ e ) {
 		if ( *prog ) {
 			test_compile( *prog, *e, static_cast<int>( prog - begin( progCompileErr ) ) );
+		}
+	}
+TUT_TEARDOWN()
+
+char const progExecuteErr0[] =
+	"main() {\n"
+	"\tv = 1;\n"
+	"\tx = v + 1.5;\n"
+	"\treturn ( 0 );\n"
+	"}\n"
+;
+
+char const progExecuteErr1[] =
+	"main() {\n"
+	"\tv = 1;\n"
+	"\tx = v - 1.5;\n"
+	"\treturn ( 0 );\n"
+	"}\n"
+;
+
+char const progExecuteErr2[] =
+	"main() {\n"
+	"\tv = 1;\n"
+	"\tx = v * 1.5;\n"
+	"\treturn ( 0 );\n"
+	"}\n"
+;
+
+char const progExecuteErr3[] =
+	"main() {\n"
+	"\tv = 1;\n"
+	"\tx = v / 1.5;\n"
+	"\treturn ( 0 );\n"
+	"}\n"
+;
+
+char const progExecuteErr4[] =
+	"main() {\n"
+	"\tv = 1;\n"
+	"\tx = v % 1.5;\n"
+	"\treturn ( 0 );\n"
+	"}\n"
+;
+
+char const progExecuteErr5[] =
+	"main() {\n"
+	"\tv = 1;\n"
+	"\tx = v ^ 1.5;\n"
+	"\treturn ( 0 );\n"
+	"}\n"
+;
+
+char const progExecuteErr6[] =
+	"main() {\n"
+	"\tv = 2;\n"
+	"\tx = v ^ 2.5;\n"
+	"\treturn ( 0 );\n"
+	"}\n"
+;
+
+char const progExecuteErr7[] =
+	"main() {\n"
+	"\tv = 1;\n"
+	"\tx = v == 1.5;\n"
+	"\treturn ( 0 );\n"
+	"}\n"
+;
+
+char const progExecuteErr8[] =
+	"main() {\n"
+	"\tv = 1;\n"
+	"\tx = v != 1.5;\n"
+	"\treturn ( 0 );\n"
+	"}\n"
+;
+
+char const progExecuteErr9[] =
+	"main() {\n"
+	"\tv = 1;\n"
+	"\tx = v < 1.5;\n"
+	"\treturn ( 0 );\n"
+	"}\n"
+;
+
+char const progExecuteErr10[] =
+	"main() {\n"
+	"\tv = 1;\n"
+	"\tx = v > 1.5;\n"
+	"\treturn ( 0 );\n"
+	"}\n"
+;
+
+void tut_yaal_tools_hhuginn::test_execute( prog_src_t prog_, int const err_[3], int index_ ) {
+	HStringStream prog( prog_ );
+	HHuginn h;
+	h.load( prog );
+	h.preprocess();
+	ENSURE( "failed to parse valid", h.parse() );
+	ENSURE( "failed to compile valid", h.compile() );
+	clog << "executing: " << index_ << endl;
+	ENSURE_NOT( "executed invalid", h.execute() );
+	ENSURE_EQUALS( "reporting error position failed " + to_string( index_ ), h.error_position(), err_[0] );
+	ENSURE_EQUALS( "reporting error line failed " + to_string( index_ ), h.error_coordinate().line(), err_[1] );
+	ENSURE_EQUALS( "reporting error column failed " + to_string( index_ ), h.error_coordinate().column(), err_[2] );
+	clog << h.error_message() << endl;
+}
+
+TUT_UNIT_TEST( "report execution error" )
+	prog_src_t progExecuteErr[] = {
+		progExecuteErr0,
+		progExecuteErr1,
+		progExecuteErr2,
+		progExecuteErr3,
+		progExecuteErr4,
+		progExecuteErr5,
+		progExecuteErr6,
+		progExecuteErr7,
+		progExecuteErr8,
+		progExecuteErr9,
+		progExecuteErr10,
+		NULL
+	};
+	int const err[][3] = {
+		{ 24, 3, 8 },   // 0
+		{ 24, 3, 8 },   // 1
+		{ 24, 3, 8 },   // 2
+		{ 24, 3, 8 },   // 3
+		{ 24, 3, 8 },   // 4
+		{ 24, 3, 8 },   // 5
+		{ 24, 3, 8 },   // 6
+		{ 24, 3, 8 },  // 7
+		{ 24, 3, 8 },  // 8
+		{ 24, 3, 8 },   // 9
+		{ 24, 3, 8 },   // 10
+		{ 0, 0, 0 }
+	};
+	int const (*e)[3]( err );
+	for ( prog_src_t* prog( begin( progExecuteErr ) ), * progEnd( end( progExecuteErr ) ); prog != progEnd; ++ prog, ++ e ) {
+		if ( *prog ) {
+			test_execute( *prog, *e, static_cast<int>( prog - begin( progExecuteErr ) ) );
 		}
 	}
 TUT_TEARDOWN()
