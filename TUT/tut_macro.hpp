3563a925ffb3584a0f427fcfc2b35e19fd96741f
Marcin Konarski <marcin.konarski@codestation.org>
2015-04-23 20:16:39 +0200

    TUT: Workaround GCC bug regarding stringification of UDLs.

--- TUT/tut_macro.hpp
+++ TUT/tut_macro.hpp
@@ -23,15 +23,25 @@
 #	error Tress redefines TIME_CONSTRAINT macro.
 #endif /* #ifdef TIME_CONSTRAINT */
 
-#define ENSURE( ... ) do { this->set_current_line( __LINE__ ); tut::ensure_real( __FILE__, __LINE__, # __VA_ARGS__, __VA_ARGS__ ); this->set_current_line( __LINE__ + 1 ); } while ( false )
-#define ENSURE_NOT( ... ) do { this->set_current_line( __LINE__ );  tut::ensure_not_real( __FILE__, __LINE__, # __VA_ARGS__, __VA_ARGS__ ); this->set_current_line( __LINE__ + 1 ); } while ( false )
-#define ENSURE_EQUALS( ... ) do { this->set_current_line( __LINE__ );  tut::ensure_equals_real( __FILE__, __LINE__, # __VA_ARGS__, __VA_ARGS__ ); this->set_current_line( __LINE__ + 1 ); } while ( false )
-#define ENSURE_LESS( ... ) do { this->set_current_line( __LINE__ );  tut::ensure_less_real( __FILE__, __LINE__, # __VA_ARGS__, __VA_ARGS__ ); this->set_current_line( __LINE__ + 1 ); } while ( false )
-#define ENSURE_GREATER( ... ) do { this->set_current_line( __LINE__ );  tut::ensure_greater_real( __FILE__, __LINE__, # __VA_ARGS__, __VA_ARGS__ ); this->set_current_line( __LINE__ + 1 ); } while ( false )
-#define ENSURE_LESS_OR_EQUAL( ... ) do { this->set_current_line( __LINE__ );  tut::ensure_less_or_equal_real( __FILE__, __LINE__, # __VA_ARGS__, __VA_ARGS__ ); this->set_current_line( __LINE__ + 1 ); } while ( false )
-#define ENSURE_GREATER_OR_EQUAL( ... ) do { this->set_current_line( __LINE__ );  tut::ensure_greater_or_equal_real( __FILE__, __LINE__, # __VA_ARGS__, __VA_ARGS__ ); this->set_current_line( __LINE__ + 1 ); } while ( false )
-#define ENSURE_DISTANCE( ... ) do { this->set_current_line( __LINE__ );  tut::ensure_distance_real( __FILE__, __LINE__, # __VA_ARGS__, __VA_ARGS__ ); this->set_current_line( __LINE__ + 1 ); } while ( false )
-#define ENSURE_ERRNO( ... ) do { this->set_current_line( __LINE__ );  tut::ensure_errno_real( __FILE__, __LINE__, # __VA_ARGS__, __VA_ARGS__ ); this->set_current_line( __LINE__ + 1 ) }; while ( false )
+/*
+ * Workaround for GCC bug: 65860.
+ */
+#ifdef __clang__
+#define UDL_STRINGIFY( ... ) __VA_ARGS__
+#else
+#define UDL_STRINGIFY_REAL( ... ) # __VA_ARGS__
+#define UDL_STRINGIFY( ... ) UDL_STRINGIFY_REAL( __VA_ARGS__ )
+#endif
+
+#define ENSURE( ... ) do { this->set_current_line( __LINE__ ); tut::ensure_real( __FILE__, __LINE__, UDL_STRINGIFY( # __VA_ARGS__ ), __VA_ARGS__ ); this->set_current_line( __LINE__ + 1 ); } while ( false )
+#define ENSURE_NOT( ... ) do { this->set_current_line( __LINE__ );  tut::ensure_not_real( __FILE__, __LINE__, UDL_STRINGIFY( # __VA_ARGS__ ), __VA_ARGS__ ); this->set_current_line( __LINE__ + 1 ); } while ( false )
+#define ENSURE_EQUALS( ... ) do { this->set_current_line( __LINE__ );  tut::ensure_equals_real( __FILE__, __LINE__, UDL_STRINGIFY( # __VA_ARGS__ ), __VA_ARGS__ ); this->set_current_line( __LINE__ + 1 ); } while ( false )
+#define ENSURE_LESS( ... ) do { this->set_current_line( __LINE__ );  tut::ensure_less_real( __FILE__, __LINE__, UDL_STRINGIFY( # __VA_ARGS__ ), __VA_ARGS__ ); this->set_current_line( __LINE__ + 1 ); } while ( false )
+#define ENSURE_GREATER( ... ) do { this->set_current_line( __LINE__ );  tut::ensure_greater_real( __FILE__, __LINE__, UDL_STRINGIFY( # __VA_ARGS__ ), __VA_ARGS__ ); this->set_current_line( __LINE__ + 1 ); } while ( false )
+#define ENSURE_LESS_OR_EQUAL( ... ) do { this->set_current_line( __LINE__ );  tut::ensure_less_or_equal_real( __FILE__, __LINE__, UDL_STRINGIFY( # __VA_ARGS__ ), __VA_ARGS__ ); this->set_current_line( __LINE__ + 1 ); } while ( false )
+#define ENSURE_GREATER_OR_EQUAL( ... ) do { this->set_current_line( __LINE__ );  tut::ensure_greater_or_equal_real( __FILE__, __LINE__, UDL_STRINGIFY( # __VA_ARGS__ ), __VA_ARGS__ ); this->set_current_line( __LINE__ + 1 ); } while ( false )
+#define ENSURE_DISTANCE( ... ) do { this->set_current_line( __LINE__ );  tut::ensure_distance_real( __FILE__, __LINE__, UDL_STRINGIFY( # __VA_ARGS__ ), __VA_ARGS__ ); this->set_current_line( __LINE__ + 1 ); } while ( false )
+#define ENSURE_ERRNO( ... ) do { this->set_current_line( __LINE__ );  tut::ensure_errno_real( __FILE__, __LINE__, UDL_STRINGIFY( # __VA_ARGS__ ), __VA_ARGS__ ); this->set_current_line( __LINE__ + 1 ) }; while ( false )
 #define ENSURE_THROW( msg, code, type ) do { try { this->set_current_line( __LINE__ ); code; throw failure( __FILE__, __LINE__, msg ); } catch ( type const& ) { this->set_current_line( __LINE__ + 1 ); } } while ( false )
 #define ENSURE_THROW_AUTO( code, type ) do { try { this->set_current_line( __LINE__ ); code; throw failure( __FILE__, __LINE__, "Expected exception `" ##type "' not thrown from: `" ##code "'." ); } catch ( type const& ) { this->set_current_line( __LINE__ + 1 ); } } while ( false )
 #define FAIL( msg ) tut::fail_real( __FILE__, __LINE__, ( msg ) )
