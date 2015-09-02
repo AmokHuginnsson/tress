f51b7690f66946337039c795ba975a08e0a08fbf
Marcin Konarski <marcin.konarski@codestation.org>
2015-09-02 05:31:40 +0200

    HHuginn: Test making variable with restricted name.

--- src/tut_yaal_tools_hhuginn.cxx
+++ src/tut_yaal_tools_hhuginn.cxx
@@ -1014,6 +1014,12 @@ char const progCompileErr52[] =
 	"}"
 ;
 
+char const progCompileErr53[] =
+	"main(){\n"
+	"if = 0;\n"
+	"}"
+;
+
 void tut_yaal_tools_hhuginn::test_compile( prog_src_t prog_, int const err_[3], int index_ ) {
 	HStringStream prog( prog_ );
 	HHuginn h;
@@ -1083,6 +1089,7 @@ TUT_UNIT_TEST( "report compilation error" )
 		progCompileErr50,
 		progCompileErr51,
 		progCompileErr52,
+		progCompileErr53,
 		NULL
 	};
 	int const err[][3] = {
@@ -1139,6 +1146,7 @@ TUT_UNIT_TEST( "report compilation error" )
 		{ 12, 2, 5 },   // 50
 		{ 14, 2, 7 },   // 51
 		{ 14, 2, 7 },   // 52
+		{ 8, 2, 1 },    // 53
 		{ 0, 0, 0 }
 	};
 	int const (*e)[3]( err );
