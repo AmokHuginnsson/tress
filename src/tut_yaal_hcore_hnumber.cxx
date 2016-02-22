5c85badf8023b50615c35b544026609620ad7d1c
Marcin Konarski <marcin.konarski@codestation.org>
2016-02-22 20:27:22 +0100

    HNumber: Test buggy power operation x^1.

--- src/tut_yaal_hcore_hnumber.cxx
+++ src/tut_yaal_hcore_hnumber.cxx
@@ -1114,6 +1114,7 @@ TUT_TEARDOWN()
 TUT_UNIT_TEST( "power" )
 	ENSURE_EQUALS( "power failed 0^7", HNumber( "0" ) ^ 7, HNumber( 0 ) );
 	ENSURE_EQUALS( "power failed 7^0", HNumber( "7" ) ^ 0, HNumber( 1 ) );
+	ENSURE_EQUALS( "power failed 7^1", HNumber( "7" ) ^ 1, HNumber( 7 ) );
 	ENSURE_EQUALS( "power failed 7^7", HNumber( "7" ) ^ 7, HNumber( "823543" ) );
 	ENSURE_EQUALS( "power failed 7^-7", HNumber( "7" ) ^ -7, HNumber( "0.0000012142656789020124025096443051546792335069328498936910398121288141602806410837078331064680289918" ) );
 	ENSURE_EQUALS( "power failed -7^7", HNumber( "-7" ) ^ 7, HNumber( "-823543" ) );
