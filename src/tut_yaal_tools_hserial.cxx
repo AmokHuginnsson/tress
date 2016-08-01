ea608ae8d57697df6677bb0ce50bebc00b667e18
Marcin Konarski <marcin.konarski@codestation.org>
2016-08-01 11:42:12 +0200

    HSerial: Add test suite.

---
+++ src/tut_yaal_tools_hserial.cxx
@@ -0,0 +1,76 @@
+/*
+---            `tress' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski             ---
+
+	tut_yaal_tools_hserial.cxx - this file is integral part of `tress' project.
+
+  i.  You may not make any changes in Copyright information.
+  ii. You must attach Copyright information to any part of every copy
+      of this software.
+
+Copyright:
+
+ You can use this software free of charge and you can redistribute its binary
+ package freely but:
+  1. You are not allowed to use any part of sources of this software.
+  2. You are not allowed to redistribute any part of sources of this software.
+  3. You are not allowed to reverse engineer this software.
+  4. If you want to distribute a binary package of this software you cannot
+     demand any fees for it. You cannot even demand
+     a return of cost of the media or distribution (CD for example).
+  5. You cannot involve this software in any commercial activity (for example
+     as a free add-on to paid software or newspaper).
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
+*/
+
+#include <TUT/tut.hpp>
+
+#include <yaal/tools/hserial.hxx>
+#include <yaal/hcore/hlog.hxx>
+#include <yaal/tools/tools.hxx>
+#include <yaal/tools/hpipedchild.hxx>
+M_VCSID( "$Id: " __ID__ " $" )
+#include "tut_helpers.hxx"
+
+using namespace tut;
+using namespace yaal;
+using namespace yaal::hcore;
+using namespace yaal::tools;
+using namespace tress::tut_helpers;
+
+namespace tut {
+
+struct tut_yaal_tools_hserial : public simple_mock<tut_yaal_tools_hserial> {
+	typedef simple_mock<tut_yaal_tools_hserial> base_type;
+	virtual ~tut_yaal_tools_hserial(){}
+	HPipedChild _socat;
+	HString _path;
+	tut_yaal_tools_hserial( void )
+		: _socat()
+		, _path() {
+		try {
+			_socat.spawn( "/usr/bin/socat", { "-d", "-d", "pty,raw,echo=0", "pty,raw,echo=0" } );
+			_socat.err().read_until( _path );
+			int long pathStart( _path.find_last( ' ' ) );
+			M_ENSURE( pathStart != HString::npos );
+			_path.shift_left( pathStart + 1 );
+		} catch ( HPipedChildException const& e ) {
+			hcore::log << e.what() << endl;
+		}
+	}
+};
+TUT_TEST_GROUP( tut_yaal_tools_hserial, "yaal::tools::HSerial" );
+
+TUT_UNIT_TEST( "default constructor" )
+	if ( ! _socat.is_running() ) {
+		SKIP( "failed to start sockat" );
+	}
+	HSerial s( _path );
+	ENSURE_NOT( "bad validity state before open", s.is_valid() );
+	s.open();
+	ENSURE( "bad validity state after open", s.is_valid() );
+TUT_TEARDOWN()
+
+}
+
