aac971d28440d1d47de84e3c21f046df5e075721
Marcin Konarski <marcin.konarski@codestation.org>
2016-11-29 22:53:25 +0100

    HScheduledAsyncCaller: Test call_in, and flush.

--- src/tut_yaal_tools_hscheduledasynccaller.cxx
+++ src/tut_yaal_tools_hscheduledasynccaller.cxx
@@ -60,8 +60,9 @@ struct tut_yaal_tools_hscheduledasynccaller : public simple_mock<tut_yaal_tools_
 
 TUT_TEST_GROUP( tut_yaal_tools_hscheduledasynccaller, "yaal::tools::HScheduledAsyncCaller" );
 
-TUT_UNIT_TEST( "functional test" )
+TUT_UNIT_TEST( "call_at" )
 	int const expect( 7 );
+	_val = 0;
 	_sac.call_at(
 		now_local().mod_second( 1 ),
 		call( &tut_yaal_tools_hscheduledasynccaller::action, this, expect )
@@ -78,11 +79,62 @@ TUT_UNIT_TEST( "functional test" )
 		}
 		HLock l( _mutex );
 		if ( _val == expect ) {
+			ENSURE( "too soon", i > 0 );
 			break;
 		}
 	}
 	HLock l( _mutex );
 	ENSURE_EQUALS( "scheduled call misfired", _val, expect );
+TUT_TEARDOWN()
+
+TUT_UNIT_TEST( "call_in" )
+	int const expect( 7 );
+	_val = 0;
+	_sac.call_in(
+		duration( 1, time::UNIT::SECOND ),
+		call( &tut_yaal_tools_hscheduledasynccaller::action, this, expect )
+	);
+	ENSURE_EQUALS( "scheduled call did not wait", _val, 0 );
+#ifdef _MSC_VER
+	static int const WAIT( 16 );
+#else /* #ifdef _MSC_VER */
+	static int const WAIT( 8 );
+#endif /* #else #ifdef _MSC_VER */
+	for ( int i( 0 ); i < WAIT; ++ i ) {
+		if ( ! sleep_for( duration( 500, time::UNIT::MILLISECOND ) ) ) {
+			log_trace << "sleep interrupted!" << endl;
+		}
+		HLock l( _mutex );
+		if ( _val == expect ) {
+			ENSURE( "too soon", i > 0 );
+			break;
+		}
+	}
+	HLock l( _mutex );
+	ENSURE_EQUALS( "scheduled call misfired", _val, expect );
+TUT_TEARDOWN()
+
+TUT_UNIT_TEST( "flush" )
+	_val = 0;
+	_sac.call_in(
+		duration( 1, time::UNIT::SECOND ),
+		call( &tut_yaal_tools_hscheduledasynccaller::action, this, 7 )
+	);
+	ENSURE_EQUALS( "scheduled call did not wait", _val, 0 );
+#ifdef _MSC_VER
+	static int const WAIT( 8 );
+#else /* #ifdef _MSC_VER */
+	static int const WAIT( 4 );
+#endif /* #else #ifdef _MSC_VER */
+	if ( ! sleep_for( duration( 100, time::UNIT::MILLISECOND ) ) ) {
+		log_trace << "sleep interrupted!" << endl;
+	}
+	_sac.flush( this );
+	if ( ! sleep_for( duration( WAIT, time::UNIT::SECOND ) ) ) {
+		log_trace << "sleep interrupted!" << endl;
+	}
+	HLock l( _mutex );
+	ENSURE_EQUALS( "scheduled call did not flush", _val, 0 );
 TUT_TEARDOWN()
 
 }
