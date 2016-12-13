4cd73268222721eb930ee7c9443e64dd7bbe1b69
Marcin Konarski <marcin.konarski@codestation.org>
2016-12-13 07:43:35 +0100

    HHuginn: Fix socket test.

--- src/tut_yaal_tools_hhuginn_packages.cxx
+++ src/tut_yaal_tools_hhuginn_packages.cxx
@@ -1653,46 +1653,47 @@ TUT_UNIT_TEST( "Network" )
 	);
 	ENSURE( "Network.resolve falied", ( res == "\"127.0.0.1\"" ) || ( res == "\"0.0.0.0\"" ) );
 	HThread serverRunner;
+	HIODispatcher iod( 11, 1000 );
+	HSocket::ptr_t serv( make_pointer<HSocket>( HSocket::TYPE::NETWORK | HSocket::TYPE::BLOCKING, 1 ) );
+	serv->listen( "127.0.0.1", OBSCURE_PORT );
+	iod.register_file_descriptor_handler(
+		serv,
+		HIODispatcher::callback_t(
+			[this, &iod, &serv]( HIODispatcher::stream_t& ) {
+				clog << "ENTER: acceptor" << endl;
+				HSocket::ptr_t client( serv->accept() );
+				iod.register_file_descriptor_handler(
+					client,
+					HIODispatcher::callback_t(
+						[&iod]( HIODispatcher::stream_t& stream_ ) {
+							clog << "ENTER: message hangler" << endl;
+							hcore::HString line;
+							if ( stream_->read_until( line ) > 0 ) {
+								clog << "serv got: [" << line << "]" << endl;
+								reverse( line.begin(), line.end() );
+								*stream_ << line << endl;
+							} else {
+								iod.unregister_file_descriptor_handler( stream_ );
+							}
+							clog << "LEAVE: message hangler" << endl;
+						}
+					)
+				);
+				clog << "LEAVE: acceptor" << endl;
+			}
+		)
+	);
 	serverRunner.spawn(
 		HThread::call_t(
-			[this]() -> void {
+			[this, &iod]() -> void {
 				try {
 					clog << "ENTER: server" << endl;
-					HSocket::ptr_t serv( make_pointer<HSocket>( HSocket::TYPE::NETWORK | HSocket::TYPE::BLOCKING, 1 ) );
-					serv->listen( "127.0.0.1", OBSCURE_PORT );
-					HIODispatcher iod( 11, 1000 );
-					iod.register_file_descriptor_handler(
-						serv,
-						HIODispatcher::callback_t(
-							[this, &iod, &serv]( HIODispatcher::stream_t& ) {
-								clog << "ENTER: acceptor" << endl;
-								HSocket::ptr_t client( serv->accept() );
-								iod.register_file_descriptor_handler(
-									client,
-									HIODispatcher::callback_t(
-										[]( HIODispatcher::stream_t& stream_ ) {
-											clog << "ENTER: message hangler" << endl;
-											hcore::HString line;
-											getline( *stream_, line );
-											clog << "serv got: [" << line << "]" << endl;
-											reverse( line.begin(), line.end() );
-											*stream_ << line << endl;
-											clog << "LEAVE: message hangler" << endl;
-										}
-									)
-								);
-								clog << "LEAVE: acceptor" << endl;
-							}
-						)
-					);
 					iod.run();
-					clog << "RUNNING: server" << endl;
-					iod.stop();
 					clog << "LEAVE: server" << endl;
 				} catch ( HException const& e ) {
-					cerr << e.what() << endl;
+					cerr << "exception caught: " << e.what() << endl;
 				} catch ( std::exception const& e ) {
-					cerr << e.what() << endl;
+					cerr << "exception caught: " << e.what() << endl;
 				} catch ( ... ) {
 					cerr << "unknown exception type" << endl;
 				}
@@ -1711,6 +1712,7 @@ TUT_UNIT_TEST( "Network" )
 		),
 		"\"!tseb si nniguH\n\""
 	);
+	iod.stop();
 	serverRunner.finish();
 TUT_TEARDOWN()
 
