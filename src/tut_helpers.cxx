70b400715833aa3d6489760f940570222ada9b52
Marcin Konarski <marcin.konarski@codestation.org>
2017-05-30 13:10:39 +0200

    On FreeBSD utf8 is invalid encoding name, one shall use utf-8 name.

--- src/tut_helpers.cxx
+++ src/tut_helpers.cxx
@@ -81,7 +81,7 @@ HString const& _outputEncoding_( output_encoding() );
 template<>
 std::string lexical_cast( yaal::hcore::HString const& str_ ) {
 	yaal::hcore::HCharacterEncodingConversionResult out;
-	yaal::hcore::HCharacterEncodingConverter encConv( "utf8", _outputEncoding_ );
+	yaal::hcore::HCharacterEncodingConverter encConv( "utf-8", _outputEncoding_ );
 	yaal::hcore::HUTF8String utf8( str_ );
 	encConv.convert( utf8.c_str(), utf8.byte_count(), out );
 	return ( out.size() > 0 ? out.c_str() : "" );
