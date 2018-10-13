b0199136e6aba3b63660dcc0be7456b2388a35d7
Marcin Konarski <marcin.konarski@codestation.org>
2018-10-13 13:57:54 +0200

    HHuginn: Add suite for Terminal package.

---
+++ src/tut_yaal_tools_hhuginn_terminal.cxx
@@ -0,0 +1,110 @@
+/* Read tress/LICENSE.md file for copyright and licensing information. */
+
+#include <TUT/tut.hpp>
+
+#include <yaal/tools/hterminal.hxx>
+#include <yaal/tools/ansi.hxx>
+M_VCSID( "$Id: " __ID__ " $" )
+#include "tut_helpers.hxx"
+
+#include "tut_yaal_tools_hhuginn_base.hxx"
+
+using namespace tut;
+using namespace yaal;
+using namespace yaal::hcore;
+using namespace yaal::tools;
+using namespace tress::tut_helpers;
+
+namespace tut {
+
+struct tut_yaal_tools_hhuginn_terminal : public tress::tut_yaal_tools_hhuginn_base {
+	virtual ~tut_yaal_tools_hhuginn_terminal( void ) {}
+};
+
+TUT_TEST_GROUP( tut_yaal_tools_hhuginn_terminal, "yaal::tools::HHuginn.Terminal" );
+
+TUT_UNIT_TEST( "is_valid" )
+	ENSURE_EQUALS(
+		"Terminal.is_valid failed",
+		execute(
+			"import Terminal as term;\n"
+			"main(){\n"
+			"return(term.is_valid());\n"
+			"}"
+		),
+		HTerminal::get_instance().exists() ? "true" : "false"
+	);
+TUT_TEARDOWN()
+
+TUT_UNIT_TEST( "lines columns" )
+	HTerminal& term( HTerminal::get_instance() );
+	if ( term.exists() ) {
+		ENSURE_EQUALS(
+			"Terminal.lines and Terminal.columns failed",
+			execute(
+				"import Terminal as term;\n"
+				"main(){\n"
+				"return((term.lines(),term.columns()));\n"
+				"}"
+			),
+			"("_ys.append( term.size().first ).append( ", " ).append( term.size().second ).append( ')' )
+		);
+	}
+TUT_TEARDOWN()
+
+TUT_UNIT_TEST( "attribute" )
+	HTerminal& term( HTerminal::get_instance() );
+	ENSURE_EQUALS(
+		"Terminal.attribute failed",
+		execute(
+			"import Terminal as term;\n"
+			"main(){\n"
+			"return(term.attribute(term.ATTRIBUTE.BOLD,\"bold\"));\n"
+			"}"
+		),
+		term.exists() ? "\""_ys.append( *ansi::bold ).append( "bold" ).append( *ansi::reset ).append( '"' ) : "\"bold\""
+	);
+TUT_TEARDOWN()
+
+TUT_UNIT_TEST( "color" )
+	HTerminal& term( HTerminal::get_instance() );
+	ENSURE_EQUALS(
+		"Terminal.color failed",
+		execute(
+			"import Terminal as term;\n"
+			"main(){\n"
+			"return(term.color(term.COLOR.BRIGHTBLUE,\"brightblue\"));\n"
+			"}"
+		),
+		term.exists() ? "\""_ys.append( *ansi::brightblue ).append( "brightblue" ).append( *ansi::reset ).append( '"' ) : "\"brightblue\""
+	);
+TUT_TEARDOWN()
+
+TUT_UNIT_TEST( "move" )
+	ENSURE_EQUALS(
+		"Terminal.move failed",
+		execute(
+			"import Terminal as term;\n"
+			"main(){\n"
+			"return(term.move(7,13));\n"
+			"}"
+		),
+		"\"\033[8;14H\""
+	);
+TUT_TEARDOWN()
+
+TUT_UNIT_TEST( "save, clear, restore, reset" )
+	ENSURE_EQUALS(
+		"Terminal.(save, clear, restore, reset) failed",
+		execute(
+			"import Terminal as term;\n"
+			"main(){\n"
+			"return((term.save(),term.clear(),term.restore(),term.reset()));\n"
+			"}"
+		),
+		"(\"\033[s\", \"\033[2J\", \"\033[u\", \"\033[0m\")"
+	);
+TUT_TEARDOWN()
+
+}
+
