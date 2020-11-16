ddef771b3d805b3a5e0910afc9512cd55a8e81d7
Marcin Konarski <marcin.konarski@codestation.org>
2020-11-16 05:10:24 +0100

    make: Fix env var passing on FreeBSD.

--- tress.mk
+++ tress.mk
@@ -10,7 +10,16 @@ test: $(TARGET)
 	touch ./out/glob/1/abc ./out/glob/2/abd ./out/glob/3/abe && \
 	chmod 100 ./out/glob/2 && \
 	PROC_LIMIT=1024 . _aux/set-limits.sh && \
-	$(TRESS_ENV) TZ="Europe/Warsaw" TRESSRC="tressrc" YAAL_LOG_LEVEL="info" DEFAULT_TARGET="debug" \
+	env -i $(TRESS_ENV) \
+		PATH="${PATH}" \
+		LD_LIBRARY_PATH="${LD_LIBRARY_PATH}" \
+		LC_CTYPE="${LC_CTYPE}" \
+		LC_COLLATE="${LC_COLLATE}" \
+		TZ="Europe/Warsaw" \
+		TRESSRC="tressrc" \
+		YAAL_LOG_LEVEL="info" \
+		DEFAULT_TARGET="debug" \
+		TRESS_CLOCK_QUALITY_MULTIPLIER="$${TRESS_CLOCK_QUALITY_MULTIPLIER:-1}" \
 	./build/$(if $(TARGET),$(TARGET),debug)/tress/1exec -q $(TRESS_ARG) < /dev/null > /dev/null
 
 memcheck: $(TARGET)
