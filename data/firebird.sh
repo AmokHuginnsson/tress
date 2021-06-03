2cf029b68bc5b1d5d08a9cbea4f51904bb1eb61b
Marcin Konarski <marcin.konarski@codestation.org>
2021-06-03 12:57:10 +0200

    data: Support `Firebird` version 4.

--- data/firebird.sh
+++ data/firebird.sh
@@ -1,12 +1,15 @@
 #! /bin/sh
 
-VER=`echo | isql-fb -q -z | awk -F '[ .-]' '/Version:/{print $4}'`
+version=$(echo | isql-fb -q -z | awk -F '[ .-]' '/Version:/{print $4}')
 
-DB="security2.fdb"
-if [ "x${VER}" = "xV3" ] ; then
-	DB="template"
+database="security2.fdb"
+if [ "x${version}" = "xV4" ] ; then
+	version="V3"
+fi
+if [ "x${version}" = "xV3" ] ; then
+	database="template"
 fi
 
 set -x
-exec isql-fb -e -r 'RDB$ADMIN' -u 'root' -p 'r00t' "localhost:${DB}" < data/firebird${VER}.sql
+exec isql-fb -e -r 'RDB$ADMIN' -u 'root' -p 'r00t' "localhost:${database}" < data/firebird${version}.sql
 
