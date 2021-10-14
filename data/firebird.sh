7ebfc52bd476b44e141fca9f4cb4a490443f6f91
Marcin Konarski <marcin.konarski@codestation.org>
2021-10-14 03:28:51 +0200

    data: Ignore Firebirds client execution status.

--- data/firebird.sh
+++ data/firebird.sh
@@ -10,6 +10,7 @@ if [ "x${version}" = "xV3" ] ; then
 	database="template"
 fi
 
-set -x
-exec isql-fb -e -r 'RDB$ADMIN' -u 'root' -p 'r00t' "localhost:${database}" < data/firebird${version}.sql
+set +e
+( set -x && isql-fb -e -r 'RDB$ADMIN' -u 'root' -p 'r00t' "localhost:${database}" < data/firebird${version}.sql )
+exit 0
 
